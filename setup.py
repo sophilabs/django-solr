452128b605f609e4deab4404cd376b24519f12fc
Pablo Ricco <pricco@gmail.com>
2011-09-08 15:46:13 -0300

    v1.0.0alpha3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-solr',
-    version='1.0.0alpha2',
+    version='1.0.0alpha3',
     description='Solr Search Engine ORM for Django',
     author='Sophilabs',
     author_email='contact@sophilabs.com',
     url='https://github.com/sophilabs/django-solr',
-    download_url='http://github.com/sophilabs/django-solr/tarball/v1.0.0alpha2#egg=django-solr-1.0.0alpha2',
+    download_url='http://github.com/sophilabs/django-solr/tarball/v1.0.0alpha3#egg=django-solr-1.0.0alpha3',
     license='BSD',
     packages=find_packages(exclude=('example')),
     classifiers=[
